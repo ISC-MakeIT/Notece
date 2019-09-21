0f3af01914221e61f8e6b8b41ba3e0eeef490a5a
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-09-22 00:28:47 +0900

    ADD src/index,newNote,Profile,Search,Setting.js

---
+++ Menu/src/index.js
@@ -0,0 +1,3 @@
+const menu = new Menu();
+const newNote = new NewNote();
+const menu = new Menu();
---
+++ Menu/src/newNote.js
@@ -0,0 +1,33 @@
+import { Menu } from './Menu';
+export default class NewNote extends Menu {
+    init() {
+        this.createLabel(
+            'url(../img/newPage.png)',
+            'newPage',
+            'url(../img/New.png)'
+        );
+        this.createLabel(
+            'url(../img/filelog.png)',
+            'newPage',
+            'url(../img/ViewLog.png)'
+        );
+    }
+    createLabel(label, labelId, btnImg) {
+        const div = document.createElement('div');
+        div.style.backgroundImage = label;
+        div.id = labelId;
+
+        const btn = document.createElement('div');
+        btn.style.backgroundImage = btnImg;
+        btn.onclick = () => {
+            if (div.id === 'newNote') {
+                // go canvas
+            } else {
+                // go filelog
+            }
+        };
+
+        this.root.appendChild(div);
+        div.appendChild(btn);
+    }
+}
