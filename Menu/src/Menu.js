7f598edb05582001c9e8fde1920b05ca645bba09
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-09-22 00:30:04 +0900

    CHANGE class Menu / Menu.js

--- Menu/src/Menu.js
+++ Menu/src/Menu.js
@@ -1,8 +1,5 @@
-class Menu {
+export default class Menu {
     constructor() {
-        // this.search = new Search();
-        // this.profile = new Profile();
-        // this.setting = new Setting();
         this.root = document.getElementById('root');
     }
     DomReset() {
@@ -13,39 +10,3 @@ class Menu {
         targetPage.init();
     }
 }
-
-class NewNote {
-    constructor() {
-        this.newPageLabel = ['../img/New.png', 'newPage', '../img/newPage.png'];
-        this.fileLogLabel;
-        this.root = document.getElementById('root');
-    }
-    init() {
-        this.createLabel(this.newPageLabel);
-    }
-    createLabel(label, labelId, btnImg) {
-        const div = document.createElement('div');
-        div.style.backgroundImage = `url(${label})`;
-        div.id = labelId;
-
-        const btn = document.createElement('div');
-        btn.style.backgroundImage = `url(${btnImg})`;
-        btn.onclick = () => {
-            if (div.id === 'newNote') {
-                // go canvas
-            } else {
-                // go filelog
-            }
-        };
-
-        this.root.appendChild(div);
-        div.appendChild(btn);
-    }
-}
-// class Search extends Menu {}
-// class Profile extends Menu {}
-// class Setting extends Menu {}
-
-const newNote = new NewNote();
-const hoge = new Menu();
-hoge.MovePage(newNote);
