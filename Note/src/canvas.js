d1f9d4f5585baa621460d611887206822711ab5f
isshun226 <51151631+isshun226@users.noreply.github.com>
2019-09-24 00:23:15 +0900

    ADD method/resize

--- Note/src/canvas.js
+++ Note/src/canvas.js
@@ -1,13 +1,16 @@
 class Canvas {
     constructor() {
-        this.parentSize = document.getElementById('canvas-wrapper');
         this.canvasSize = document.getElementById('canvas');
-        this.canvasSize.height = this.parentSize.clientHeight;
-        this.canvasSize.width = this.parentSize.clientWidth;
+        this.canvasSize.height = document.getElementById('canvas-wrapper').clientHeight;
+        this.canvasSize.width = document.getElementById('canvas-wrapper').clientWidth;
         this.board = new fabric.Canvas('canvas');
         this.board.isDrawingMode = true;
         this.board.freeDrawingBrush.color = '#2b2b2b';
         this.board.freeDrawingBrush.width = 4;
+    }
+    resize = () => {
+        this.board.setWidth(window.innerWidth);
+        this.board.setWidth(window.innerHeight);
     }
     setTools = () => {
         const trigger = document.getElementById('tag-wrapper').children;
@@ -47,3 +50,10 @@ class Canvas {
 
 const canvas = new Canvas();
 canvas.setTools();
+window.onresize = () => {
+    console.log('hi');
+    const parentSize = document.getElementById('canvas-wrapper');
+    canvas.board.setWidth(parentSize.clientWidth);
+    canvas.board.setHeight(parentSize.clientHeight);
+    console.log(parentSize.clientHeight);
+}
