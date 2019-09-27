ff007bf63c5b5fb5eb0bbf423a3124db7ffe8bba
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-09-27 10:22:16 +0900

    ADD classname

--- Menu/src/newNote.js
+++ Menu/src/newNote.js
@@ -16,9 +16,11 @@ class NewNote extends Menu {
         const div = document.createElement('div');
         div.style.backgroundImage = label;
         div.id = labelId;
+        div.className = 'post-it';
 
         const btn = document.createElement('div');
         btn.style.backgroundImage = btnImg;
+        btn.className = 'post-it-btn';
         btn.onclick = () => {
             if (div.id === 'newNote') {
                 // go canvas
