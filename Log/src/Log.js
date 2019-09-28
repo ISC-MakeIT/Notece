6c77bd2819c881c5061cbdd64c47c5ea7c1a3450
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-09-28 23:57:26 +0900

    ADD src/Log.js

--- Log/src/Log.js
+++ Log/src/Log.js
@@ -1,29 +1,65 @@
-class Memu {
+class Log {
     constructor() {
-        const root = document.getElementById('root');
+        this.root = document.getElementById('root');
+        this.tagContainer = document.getElementById('tag-container');
+        this.MenuTags = document.getElementsByClassName('tag');
+        this.detail;
+        this.noteList;
     }
-    init() {}
 
-    makeLabel() {
-        const label = document.createElement('div');
-        label.style.backgroundImage = 'url(../img/postit.png)';
-        label.style.backgroundSize = 'cover';
-        label.id = 'products-detail';
-        this.root.appendChild(label);
+    init() {
+        this.MenuTags.style.display = 'none';
+        this.createTab();
+        this.resetDom(this.root);
+        this.root.textContent = null;
+
+        const detail = document.createElement('div');
+        detail.id = 'detail';
+        this.detail = detail;
+
+        const noteList = document.createElement('div');
+        noteList.id = 'note-list';
+        this.noteList = noteList;
+
+        this.createTab('url()', 'myNote');
+        this.createTab('url()', 'watchNote');
     }
-    makeProducts(JSON) {
-        const product = document.createElement('div');
-        product.style.backgroundImage = JSON.img;
-        this.root.appendChild(product);
-        product.onclick = () => {
-            const target = document.getElementById('products-detail');
-            const tag = document.createElement('div');
-            const title = document.createElement('div');
-            tag.innerHTML = JSON.tag;
-            title.innerHTML = JSON.title;
-            target.appendChild(product);
-            target.appendChild(tag);
-            target.appendChild(title);
-        };
+
+    createTab(tabImg, tabId) {
+        const tab = document.createElement('div');
+        tab.style.backgroundImage = tabImg;
+        tab.id = tabId;
+        tab.class = 'tab';
+        this.tagContainer.appendChild(tab);
+    }
+
+    // noteDataのところをurlにしてaxiosでjson取り出せるようにする
+    displayNoteList(noteData) {
+        this.detail.textContent = null;
+        this.noteList.textContent = null;
+        for (let i = 0; i < noteData.length; i++) {
+            const div = document.createElement('div');
+            div.style.backgroundImage = noteData[i].image;
+            div.className = 'notes';
+            this.noteList.appendChild(div);
+
+            div.addEventListener('click', () => {
+                const img = document.createElement('div');
+                img.style.backgroundImage = noteData[i].image;
+                img.id = 'note-thumbnail';
+                this.detail.appendChild(img);
+
+                const noteName = document.createElement('p');
+                noteName.innerText = noteData[i].name;
+                noteName.id = 'note-name';
+                this.detail.appendChild(noteName);
+
+                const noteExplanation = document.createElement('p');
+                noteExplanation.innerText = noteData[i].explanation;
+                noteExplanation.id = 'note-explanation';
+                this.detail.appendChild(noteExplanation);
+                // tag未実装
+            });
+        }
     }
 }
