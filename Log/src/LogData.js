cf4713e9b37101a0149f3e64445fb56d72276fb3
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-09-28 23:58:01 +0900

    ADD src/LogData.js

---
+++ Log/src/LogData.js
@@ -0,0 +1,43 @@
+// logには自分のデータしかないから自分のIDをプライマリキーすればよさそう
+const Mydata = [
+    {
+        image: 'url()',
+        date: '2019/05/05',
+        name: 'itika',
+        tag: 'kawaii',
+        explanation:
+            '長女。以前撮ったホラー映画の続編決定。死んでいるのでフォロー来ず。本を読むなら小説。'
+    },
+    {
+        image: 'url()',
+        date: '2019/05/05',
+        name: 'nino',
+        tag: 'kawaii',
+        explanation:
+            '次女。制服の衣替えという概念を失っているので一年中カーディガン。本を読むならファッション誌。'
+    },
+    {
+        image: 'url()',
+        date: '2019/05/05',
+        name: 'miku',
+        tag: 'kawaii',
+        explanation:
+            '三女。中野家朝食エンドレスパン祭りの犯人。本を読むなら自叙伝。'
+    },
+    {
+        image: 'url()',
+        date: '2019/05/05',
+        name: 'yotuba',
+        tag: 'kawaii',
+        explanation:
+            '四女。映画村で買った木刀を気に入り持ち歩いていた所を職質される。本を読むなら漫画。'
+    },
+    {
+        image: 'url()',
+        date: '2019/05/05',
+        name: 'miku',
+        tag: 'kawaii',
+        explanation:
+            '五女。大人の下着は念のため押し入れの一番奥に収納中。本を読むならガイドブック。'
+    }
+];
