c352443a5cb842493a4eda4b31808079ee68fc0a
isshun226 <51151631+isshun226@users.noreply.github.com>
2019-09-30 03:14:49 +0900

    ADD src/DownloadMenu.js  method/action

--- Note/src/DownloadMenu.js
+++ Note/src/DownloadMenu.js
@@ -1,15 +1,29 @@
 import Tool from './Tool';
 class DownloadMenu extends Tool {
+    action() {
+        const wrapper = document.getElementById('tag-wrapper');
+        const menuContainer = document.createElement('div');
+        menuContainer.className = 'js-menuContainer';
+        const downloadMenu = document.createElement('div');
+        downloadMenu.id = 'js-downloadMenu';
+        wrapper.appendChild(menuContainer);
+        menuContainer.appendChild('menuContents');
+    }
+    convertToPNG() {
+        const a = document.createElement('a');
+        a.href = canvas.board.toDataURL('image/png');
+        a.download = 'YourNote.png';
+        a.click();
+    }
+    download() {
+        const check = confirm('このノートを保存しますか？');
+        if (check) {
+            const a = document.createElement('a');
+            a.href = canvas.board.toDataURL('image/png', 1);
+            a.download = "yourNote.png";
+            a.click();
+        }
+    }
 }
 
 export default DownloadMenu;
-
-/*download = () => {
-    const check = confirm('このノートを保存しますか？');
-    if (check) {
-        const a = document.createElement('a');
-        a.href = canvas.board.toDataURL('image/png', 1);
-        a.download = "yourNote.png";
-        a.click();
-    }
-}*/
--- Note/src/TextBoxMenu.js
+++ Note/src/TextBoxMenu.js
@@ -1,7 +1,6 @@
 import Tool from './Tool';
 class TextBoxMenu extends Tool {
     action() {
-        canvas.board.cursor = "url(../img/pen.png)";
     }
 }
 export default TextBoxMenu;
