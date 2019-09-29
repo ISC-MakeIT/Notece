7600e12006f1ff1689b2056d833ee4202d80b5e3
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-09-29 16:03:51 +0900

    CHANGE log.js / constructor etc...

--- Log/src/Log.js
+++ Log/src/Log.js
@@ -3,22 +3,25 @@ class Log {
         this.root = document.getElementById('root');
         this.tagContainer = document.getElementById('tag-container');
         this.MenuTags = document.getElementsByClassName('tag');
-        this.detail;
         this.noteList;
     }
 
     init() {
-        this.MenuTags.style.display = 'none';
-        this.createTab();
-        this.resetDom(this.root);
+        for (let i = 0; i < this.MenuTags.length; i++) {
+            this.MenuTags[i].style.display = 'none';
+        }
         this.root.textContent = null;
 
+        const detailWrapper = document.createElement('div');
+        detailWrapper.id = 'detail-wrapper';
+        this.root.appendChild(detailWrapper);
         const detail = document.createElement('div');
         detail.id = 'detail';
-        this.detail = detail;
+        detailWrapper.appendChild(detail);
 
         const noteList = document.createElement('div');
         noteList.id = 'note-list';
+        this.root.appendChild(noteList);
         this.noteList = noteList;
 
         this.createTab('url()', 'myNote');
@@ -26,16 +29,16 @@ class Log {
     }
 
     createTab(tabImg, tabId) {
-        const tab = document.createElement('div');
+        const tab = document.createElement('li');
         tab.style.backgroundImage = tabImg;
         tab.id = tabId;
-        tab.class = 'tab';
+        tab.className = 'tab';
         this.tagContainer.appendChild(tab);
     }
 
     // noteDataのところをurlにしてaxiosでjson取り出せるようにする
     displayNoteList(noteData) {
-        this.detail.textContent = null;
+        document.getElementById('detail').textContent = null;
         this.noteList.textContent = null;
         for (let i = 0; i < noteData.length; i++) {
             const div = document.createElement('div');
@@ -44,20 +47,21 @@ class Log {
             this.noteList.appendChild(div);
 
             div.addEventListener('click', () => {
+                document.getElementById('detail').textContent = null;
                 const img = document.createElement('div');
                 img.style.backgroundImage = noteData[i].image;
                 img.id = 'note-thumbnail';
-                this.detail.appendChild(img);
+                document.getElementById('detail').appendChild(img);
 
                 const noteName = document.createElement('p');
                 noteName.innerText = noteData[i].name;
                 noteName.id = 'note-name';
-                this.detail.appendChild(noteName);
+                document.getElementById('detail').appendChild(noteName);
 
                 const noteExplanation = document.createElement('p');
                 noteExplanation.innerText = noteData[i].explanation;
                 noteExplanation.id = 'note-explanation';
-                this.detail.appendChild(noteExplanation);
+                document.getElementById('detail').appendChild(noteExplanation);
                 // tag未実装
             });
         }
