64959d0d4198601cfaa37fe1c14903710eb760e1
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-10-05 12:43:31 +0900

    CHANGE unredo algorithm

--- Note/src/UnRedo.js
+++ Note/src/UnRedo.js
@@ -4,14 +4,15 @@ class UnRedo {
         this.target = target;
         this.mods = 0;
         this.state = [];
+        this.isUndo = false;
         this.target.on('object:modified', () => {
-            this.updateCanvas(true);
+            this.updateCanvas();
         });
         this.target.on('path:created', () => {
-            this.updateCanvas(true);
+            this.updateCanvas();
         });
         this.target.on('object:removed', () => {
-            this.updateCanvas(true);
+            this.updateCanvas();
         });
     }
 
@@ -25,30 +26,36 @@ class UnRedo {
         });
     }
 
-    updateCanvas(savehistory) {
-        if (savehistory === true) {
-            const myjson = JSON.stringify(this.target);
-            this.state.push(myjson);
+    updateCanvas() {
+        while (this.isUndo) {
+            this.state.pop();
+            this.isUndo--;
+            this.mods = 0;
         }
+        const myjson = JSON.stringify(this.target);
+        this.state.push(myjson);
+        console.log(this.state);
     }
 
     undo() {
         if (this.mods < this.state.length) {
             this.target.clear().renderAll();
             this.target.loadFromJSON(
-                this.state[this.state.length - 1 - this.mods - 1]
+                this.state[this.state.length - this.mods - 2]
             );
+            this.isUndo = this.mods + 1;
             this.target.renderAll();
+            console.log(this.state.length - 1 - this.mods - 1);
             this.mods += 1;
         }
     }
     redo() {
         if (this.mods > 0) {
             this.target.clear().renderAll();
-            this.target.loadFromJSON(
-                this.state[this.state.length - 1 - this.mods + 1]
-            );
+            this.target.loadFromJSON(this.state[this.state.length - this.mods]);
             this.target.renderAll();
+            this.isUndo = this.mods - 1;
+            console.log(this.state.length - 1 - this.mods + 1);
             this.mods -= 1;
         }
     }
