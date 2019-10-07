f4f7f4404f2271c02330ce17237bf3ad68f7af9a
56miyano <bjgm1290561@gn.iwasaki.ac.jp>
2019-10-07 18:36:44 +0900

    ADD Profile.js addeventlester

--- Menu/src/Profile.js
+++ Menu/src/Profile.js
@@ -7,16 +7,38 @@ class Profile extends Menu {
     display() {
         this.DomReset();
 
-        const name = document.createElement('input');
-        name.type = "text";
-        name.className = "text";
+        const name = document.createElement('div');
+        name.id = "name";
+        name.innerHTML = '<input id="namebox" type="text";>';
+        name.addEventListener('keypress', () => {
+            if (window.event.keyCode == 13) {
+                const get = document.getElementById('name');
+                const nameget = document.getElementById('namebox');
+                const text = nameget.value;
+                get.innerHTML = '<div>' + text + '</div>';
+            }
+        }, false);
 
-        const comment = document.createElement('input');
-        comment.type = "textbox";
-        comment.className = "text";
+
+        const comment = document.createElement('div');
+        comment.id = "comment";
+        comment.innerHTML = '<input id="commentbox" type="text";>'
+        name.addEventListener('keypress', () => {
+            if (window.event.keyCode == 13) {
+                const set = document.getElementById('comment');
+                const commentset = document.getElementById('commentbox');
+                const textbox = commentset.value;
+                set.innerHTML = '<div>' + textbox + '</div>';
+            }
+        }, false);
+
+
+        const image = document.createElement('img');
+        image.src = "../img/profile.png";
 
         document.getElementById('contents').appendChild(name);
         document.getElementById('contents').appendChild(comment);
+        document.getElementById('contents').appendChild(image);
 
     }
 
