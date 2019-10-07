9620cc3a95b2cafed6e278db8dc000f8a84cc47f
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-10-07 15:58:26 +0900

    ADD index.js/addeventLisner event

--- Menu/src/index.js
+++ Menu/src/index.js
@@ -1,11 +1,21 @@
 import TimeLine from './TimeLine';
+import Comment from './Comment';
 
 const timeline = new TimeLine();
+const comment = new Comment();
 if (localStorage.getItem('data') === null) {
     localStorage.setItem('data', JSON.stringify(timelineLog));
 }
 const data = localStorage.getItem('data');
 timeline.display(JSON.parse(data));
+
 document.getElementById('newNote').addEventListener('click', () => {
     window.location.href = '../../Note/html/index.html';
+});
+
+document.getElementById('timeline-group').addEventListener('click', () => {
+    timeline.display(JSON.parse(data));
+});
+document.getElementById('timeline-comment').addEventListener('click', () => {
+    comment.display(timelineComment);
 });
